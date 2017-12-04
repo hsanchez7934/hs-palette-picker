d62dcda2e14101a352fcb3a99f98cb458c3b198c
Hector Sanchez <hsanchez7934@gmail.com>
2017-12-04 09:42:08 -0700

    Add test property to circle yml file

--- public/test/routes.spec.js
+++ public/test/routes.spec.js
@@ -10,9 +10,20 @@ const database = require('knex')(configuration);
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-
+  it('should return the homepage with text', () => {
+    return chai.request(server)
+    .get('/')
+    .then(response => {
+      response.should.have.status(200);
+      response.should.be.html;
+      response.res.text.should.equal('We\'re going to test all the routes!');
+    })
+    .catch(err => {
+      throw err;
+    });
+  });
 });
 
 describe('API Routes', () => {
-
+  
 });
